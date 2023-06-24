7d25cf144e9bc8259d68b833c7cf3d0d4557f02c
Rene Baine <renebaine13@gmail.com>
2023-06-25 01:07:12 +0300

    updates

--- scripts/main.js
+++ scripts/main.js
@@ -9,4 +9,9 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 
-  
+  $(".option").click(function(){
+    $(".option").removeClass("active");
+    $(this).addClass("active");
+    
+ });
+ 
