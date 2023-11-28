3699641e0133488ca705d02cd574bc32da832ba8
Rene Baine <cosir47915@fitwl.com>
2023-11-28 21:41:48 +0300

    Alot happened but it works

--- script.js
+++ script.js
@@ -16,15 +16,12 @@ document.addEventListener('DOMContentLoaded', function() {
     $(this).addClass("active");
     
  });
- 
- function toggleNavbar() {
-  var navbarLinks = document.getElementById("navbarLinks");
-  navbarLinks.style.display === "block" ? navbarLinks.style.display = "none" : navbarLinks.style.display = "block";
-}
 
-
-
-
+$('.oleez-header .dropdown').hover(function() {
+  $(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
+}, function() {
+  $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp();
+});
 
 
  //End Cursor
@@ -33,77 +30,24 @@ document.addEventListener('DOMContentLoaded', function() {
   $('body').toggleClass('hiro-nav-popup-modal-open');
 });
 
-
-$('.hiro-main-nav .nav-link[href^="#"]:not([href="#!"])').click(function(){
-  $('body').removeClass('hiro-nav-popup-modal-open');
-});
-$myCarousel = $('.carousel');
-
-function doAnimations(elems) {
-  var animEndEv = 'webkitAnimationEnd animationend';
-
-  elems.each(function () {
-    var $this = $(this),
-        $animationType = $this.data('animation');
-
-    // Add animate.css classes to
-    // the elements to be animated
-    // Remove animate.css classes
-    // once the animation event has ended
-    $this.addClass($animationType).one(animEndEv, function () {
-      $this.removeClass($animationType);
-    });
-  });
-}
-
-// Select the elements to be animated
-// in the first slide on page load
-var $firstAnimatingElems = $myCarousel.find('.carousel-item:first')
-  .find('[data-animation ^= "animated"]');
-
-// Apply the animation using the doAnimations()function
-doAnimations($firstAnimatingElems);
-
-// Attach the doAnimations() function to the
-// carousel's slide.bs.carousel event
-$myCarousel.on('slide.bs.carousel', function (e) {
-  // Select the elements to be animated inside the active slide
-  var $animatingElems = $(e.relatedTarget)
-    .find("[data-animation ^= 'animated']");
-  doAnimations($animatingElems);
+$('.oleez-header .dropdown').hover(function() {
+  $(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
+}, function() {
+  $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp();
 });
 
-function doAnimations(elems) {
-  var animEndEv = 'webkitAnimationEnd animationend';
-
-  elems.each(function () {
-    var $this = $(this),
-        $animationType = $this.data('animation');
-
-    // Add animate.css classes to
-    // the elements to be animated
-    // Remove animate.css classes
-    // once the animation event has ended
-    $this.addClass($animationType).one(animEndEv, function () {
-      $this.removeClass($animationType);
-    });
-  });
-}
-
-// Select the elements to be animated
-// in the first slide on page load
-var $firstAnimatingElems = $myCarousel.find('.carousel-item:first')
-  .find('[data-animation ^= "animated"]');
-
-// Apply the animation using the doAnimations()function
-doAnimations($firstAnimatingElems);
-
-// Attach the doAnimations() function to the
-// carousel's slide.bs.carousel event
-$myCarousel.on('slide.bs.carousel', function (e) {
-  // Select the elements to be animated inside the active slide
-  var $animatingElems = $(e.relatedTarget)
-    .find("[data-animation ^= 'animated']");
-  doAnimations($animatingElems);
+$('[data-toggle="offCanvasMenu"]').click(function() {
+  $('#offCanvasMenu').addClass('open');
 });
 
+$('[data-dismiss="offCanvasMenu"]').click(function() {
+  $(this).parent('#offCanvasMenu').removeClass('open');
+});
+
+$('[data-toggle="searchModal"]').click(function() {
+  $('#searchModal').addClass('open');
+});
+
+$('[data-dismiss="searchModal"]').click(function() {
+  $(this).parent('#searchModal').removeClass('open');
+});
