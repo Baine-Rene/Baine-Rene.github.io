3e904a4427e4352c87135fde12524f48b1e7df8c
Rene Baine <cosir47915@fitwl.com>
2023-12-04 11:00:33 +0300

    Updated navbar

--- js/main.js
+++ js/main.js
@@ -1,21 +1,77 @@
-$('.oleez-header .dropdown').hover(function() {
-  $(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
-}, function() {
-  $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp();
+$('.hiro-nav-popup-toggle').click(function(){
+  $('body').toggleClass('hiro-nav-popup-modal-open');
 });
 
-$('[data-toggle="offCanvasMenu"]').click(function() {
-  $('#offCanvasMenu').addClass('open');
+
+$('.hiro-main-nav .nav-link[href^="#"]:not([href="#!"])').click(function(){
+  $('body').removeClass('hiro-nav-popup-modal-open');
+});
+$myCarousel = $('.carousel');
+
+function doAnimations(elems) {
+  var animEndEv = 'webkitAnimationEnd animationend';
+
+  elems.each(function () {
+    var $this = $(this),
+        $animationType = $this.data('animation');
+
+    // Add animate.css classes to
+    // the elements to be animated
+    // Remove animate.css classes
+    // once the animation event has ended
+    $this.addClass($animationType).one(animEndEv, function () {
+      $this.removeClass($animationType);
+    });
+  });
+}
+
+// Select the elements to be animated
+// in the first slide on page load
+var $firstAnimatingElems = $myCarousel.find('.carousel-item:first')
+  .find('[data-animation ^= "animated"]');
+
+// Apply the animation using the doAnimations()function
+doAnimations($firstAnimatingElems);
+
+// Attach the doAnimations() function to the
+// carousel's slide.bs.carousel event
+$myCarousel.on('slide.bs.carousel', function (e) {
+  // Select the elements to be animated inside the active slide
+  var $animatingElems = $(e.relatedTarget)
+    .find("[data-animation ^= 'animated']");
+  doAnimations($animatingElems);
 });
 
-$('[data-dismiss="offCanvasMenu"]').click(function() {
-  $(this).parent('#offCanvasMenu').removeClass('open');
-});
+function doAnimations(elems) {
+  var animEndEv = 'webkitAnimationEnd animationend';
 
-$('[data-toggle="searchModal"]').click(function() {
-  $('#searchModal').addClass('open');
-});
+  elems.each(function () {
+    var $this = $(this),
+        $animationType = $this.data('animation');
 
-$('[data-dismiss="searchModal"]').click(function() {
-  $(this).parent('#searchModal').removeClass('open');
+    // Add animate.css classes to
+    // the elements to be animated
+    // Remove animate.css classes
+    // once the animation event has ended
+    $this.addClass($animationType).one(animEndEv, function () {
+      $this.removeClass($animationType);
+    });
+  });
+}
+
+// Select the elements to be animated
+// in the first slide on page load
+var $firstAnimatingElems = $myCarousel.find('.carousel-item:first')
+  .find('[data-animation ^= "animated"]');
+
+// Apply the animation using the doAnimations()function
+doAnimations($firstAnimatingElems);
+
+// Attach the doAnimations() function to the
+// carousel's slide.bs.carousel event
+$myCarousel.on('slide.bs.carousel', function (e) {
+  // Select the elements to be animated inside the active slide
+  var $animatingElems = $(e.relatedTarget)
+    .find("[data-animation ^= 'animated']");
+  doAnimations($animatingElems);
 });
