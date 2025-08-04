56198ecb3862a1978234f6db7dfa92fc8f554696
Rene Baine <renebaine13@gmail.com>
2025-08-04 16:24:29 +0300

    Added introduction title and paragraph

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@
 export default function Home() {
   return (
     <div>
-      <h1>Hi, I&apos;m Rene Baine</h1>
+      <h1 className="text-3xl font-extrabold my-5">Hi, I&apos;m Rene Baine 👋</h1>
+      <p>I’m a computer science student at Trent university. I’m the Co-founder and graphic designer at IWIP, I run my own freelance graphic design business startups and youth lead initiatives. </p>
     </div>
   );
 }
